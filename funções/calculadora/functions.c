910cc7c2990296c919502b13ec10a4bf543927de
thomazllr <guithomaz270@gmail.com>
2023-11-01 00:30:58 -0300

    test

--- funções/calculadora/functions.c
+++ funções/calculadora/functions.c
@@ -1,45 +1,29 @@
-#include <stdio.h>
 #include "functions.h"
+#include <stdio.h>
 
-float somar(float a, float b)
-{
-    return a + b;
+float somar(float a, float b) { return a + b; }
+
+float sub(float a, float b) { return a - b; }
+
+float multi(float a, float b) { return a * b; }
+
+float divi(float a, float b) {
+  if (a == 0) {
+    printf("\033[31m");
+    printf("Nao eh possivel realizar a divisao\n");
+    printf("\033[0m");
+    return 0;
+  } else {
+    return a / b;
+  }
 }
 
-float sub(float a, float b)
-{
-    return a - b;
+float changeNumber(float *a, float *b) {
+  printf("Digite 1 numero ==> ");
+  scanf("%f", a);
+  printf("Digite 2 numero ==> ");
+  scanf("%f", b);
+  return 0;
 }
 
-float multi(float a, float b)
-{
-    return a * b;
-}
-
-float divi(float a, float b)
-{
-    if (a == 0)
-    {
-        printf("\033[31m");
-        printf("Nao eh possivel realizar a divisao\n");
-        printf("\033[0m");
-        return 0;
-    }
-    else
-    {
-        return a / b;
-    }
-}
-
-float changeNumber(float *a, float *b)
-{
-    printf("Digite 1 numero ==> ");
-    scanf("%f", a);
-    printf("Digite 2 numero ==> ");
-    scanf("%f", b);
-}
-
-void printResult(float x)
-{
-    printf("O resultado da op : %.0f\n", x);
-}
+void printResult(float x) { printf("O resultado da op : %.2f\n", x); }
--- funções/calculadora/main.c
+++ funções/calculadora/main.c
@@ -3,57 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thomazllr <Hi! I'm a student at UFT>       +#+  +:+       +#+        */
+/*   By: thomazllr <thomazllr@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 22:25:09 by thomazllr         #+#    #+#             */
-/*   Updated: 2023/10/26 22:25:09 by thomazllr        ###   ########.fr       */
+/*   Updated: 2023/11/01 00:30:31 by thomazllr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "functions.h"
+#include <stdio.h>
 
 int main()
 {
 
-    float x, y, result;
-    int op;
+  float x, y, result;
+  int op;
 
-    printf("Digite dois numeros ==> ");
-    scanf("%f %f", &x, &y);
+  printf("Digite dois numeros ==> ");
+  scanf("%f %f", &x, &y);
 
-    do
+  do
+  {
+    printf("\n\n== CALCULADORA ==\n\n"
+           "O que deseja fazer\n\n?"
+           "\n\n1-Soma\n2-Sub\n3-Multi\n4-Divisao\n"
+           "5-Trocar os numeros\n6-Sair\nOption ==> ");
+    scanf("%d", &op);
+    switch (op)
     {
-        printf("\n\n== CALCULADORA ==\n\nO que deseja fazer?\n\n1-Soma\n2-Sub\n3-Multi\n4-Divisao\n5-Trocar os numeros\n6-Sair\nOption ==> ");
-        scanf("%d", &op);
-        switch (op)
-        {
-        case 1:
-            result = somar(x, y);
-            printResult(result);
-            break;
-        case 2:
-            result = sub(x, y);
-            printResult(result);
-            break;
-        case 3:
-            result = multi(x, y);
-            printResult(result);
-            break;
-        case 4:
-            result = divi(x, y);
-            if (result == 0)
-            {
-                break;
-            }
-            printResult(result);
-            break;
-        case 5:
-            changeNumber(&x, &y);
-        default:
-            break;
-        }
-    } while (op != 6);
+    case 1:
+      result = somar(x, y);
+      printResult(result);
+      break;
+    case 2:
+      result = sub(x, y);
+      printResult(result);
+      break;
+    case 3:
+      result = multi(x, y);
+      printResult(result);
+      break;
+    case 4:
+      result = divi(x, y);
+      if (result == 0)
+      {
+        break;
+      }
+      printResult(result);
+      break;
+    case 5:
+      changeNumber(&x, &y);
+    default:
+      break;
+    }
+  } while (op != 6);
 
-    return 0;
+  return 0;
 }
