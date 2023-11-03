bb18ea8224562ba0b270a5f6541e20bbffd0f49f
guilherme thomaz <guithomaz270@gmail.com>
2023-11-02 22:37:43 -0300

    add: laboratorio para testes

---
+++ lab/delay/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Iniciando...\n");
+
+    // Pausa o programa por 2 segundos
+    sleep(2);
+
+    printf("Atraso de 2 segundos\n");
+
+    return 0;
+}
