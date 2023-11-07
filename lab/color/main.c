d87be04523c144b1a5d582568db209abc3c4ef6a
guilherme thomaz <guithomaz270@gmail.com>
2023-11-07 00:13:37 -0300

    colours

---
+++ lab/color/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\033[31mTexto Vermelho\033[0m\n");
+    printf("\033[32mTexto Verde\033[0m\n");
+    printf("\033[34mTexto Azul\033[0m\n");
+    return 0;
+}
